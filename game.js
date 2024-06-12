1485a571f028d7b5ff9f0e03e740b04b4b113092
gaimn <104718139+gaimn@users.noreply.github.com>
2024-06-12 21:32:50 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!")
+const mergeButton = document.getElementById("mergeButton");
+const merge1 = document.getElementById("merge1");
+const merge2 = document.getElementById("merge2");
+mergeButton.addEventListener("click", () => {
+    console.log("Do some merging")
+    console.log("You combined "+merge1.innerHTML+" and "+merge2.innerHTML+" to make ERROR!")
+});
